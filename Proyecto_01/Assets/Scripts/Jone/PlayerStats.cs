d41e4863ea8abf0b49f016d9e371fc336856737e
Sara <smm1032@alu.ubu.es>
2024-04-22 22:05:29 +0200

    Cambios min

--- Proyecto_01/Assets/Scripts/Jone/PlayerStats.cs
+++ Proyecto_01/Assets/Scripts/Jone/PlayerStats.cs
@@ -6,12 +6,12 @@ public class PlayerStats : MonoBehaviour
 {
     // Corrupción
     static public int contadorCorr = 0;
-    static public float corrupcion = 0;
+    static public float corrupcion = 0; // Barra de corrupción
     static public int monedasCorr = 0;
 
     // Salud
     static public float saludMax = 100;
-    static public float saludActual;
+    static public float saludActual; // Barra salud
 
     private void Start()
     {
