0bbaf9bf9d88cabc38382d704a1424094f38f980
Jone <jse1007@alu.ubu.es>
2024-04-25 15:08:36 +0200

    TutoriaDiegoElOtro

---
+++ Proyecto_01/Assets/Scripts/Sara/Documento.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Sara/Doc")]
+public class Documento : test
+{
+    public int index;
+    
+}
