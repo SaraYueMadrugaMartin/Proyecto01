f5ccffbb6605de9d677d5ab0100ff8ac336bf5a1
Jonetxu10 <jse1007@alu.ubu.es>
2024-05-01 11:00:48 +0200

    ComienzoMaquinaEstado

---
+++ Proyecto_01/Assets/Scripts/Jone/Enemigos/Base/Enemy.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour, IDamageable, IEnemyMoveable
+{
+    [field: SerializeField] public float MaxHealth { get; set; } = 100f;
+    public float CurrentHealth { get; set; }
+    public Rigidbody2D RB { get; set; }
+    public bool IsFacingRight { get; set; } = true;
+
+    private void Start()
+    {
+        CurrentHealth = MaxHealth;
+
+        RB = GetComponent<Rigidbody2D>();
+    }
+
+    #region Funciones de Salud/Muerte
+    public void Damage(float damageAmount)
+    {
+        CurrentHealth -= damageAmount;
+
+        if(CurrentHealth <= 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+    #endregion
+
+    #region Funciones de Movimiento
+    public void MoveEnemy(Vector2 velocity)
+    {
+        RB.velocity = velocity;
+        CheckForLeftOrRightFacing(velocity);
+    }
+
+    public void CheckForLeftOrRightFacing(Vector2 velocity)
+    {
+        if (IsFacingRight && velocity.x < 0f)
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x, 100f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            IsFacingRight = !IsFacingRight;
+        }
+        else if (!IsFacingRight && velocity.x > 0f)
+        {
+            Vector3 rotator = new Vector3(transform.rotation.x, 0f, transform.rotation.z);
+            transform.rotation = Quaternion.Euler(rotator);
+            IsFacingRight = !IsFacingRight;
+        }
+    }
+    #endregion
+}
