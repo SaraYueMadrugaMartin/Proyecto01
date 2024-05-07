e219246c523c6f0a23f0d0bc895abfd169bf949c
Jonetxu10 <jse1007@alu.ubu.es>
2024-05-08 01:09:59 +0200

    Planta1General

---
+++ Proyecto_01/Assets/Scripts/Jone/VisibilidadPuertasFrente.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class VisibilidadPuertasFrente : MonoBehaviour
+{
+    Tilemap puertas;
+
+    void Start()
+    {
+        puertas = GetComponent<Tilemap>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Cuando un objeto entra en la zona de las puertas, estas se ponen más transparentes
+        CambiarTransparenciaColor(0.5f);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // Cuando un objeto sale de la zona de las puertas, estas vuelven a su transparencia original
+        CambiarTransparenciaColor(1f);
+    }
+
+    void CambiarTransparenciaColor(float transparencia)
+    {
+        // Obtener el color actual del objeto
+        Color colorActual = puertas.color;
+
+        // Establecer la nueva transparencia
+        colorActual.a = transparencia;
+
+        // Aplicar el nuevo color al objeto
+        puertas.color = colorActual;
+    }
+}
