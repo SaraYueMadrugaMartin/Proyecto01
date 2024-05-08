3f922faae1701aff50161328900e185e964a482f
Jone <jse1007@alu.ubu.es>
2024-05-08 18:44:48 +0200

    InicioMaquinaEstado

---
+++ Proyecto_01/Assets/Scripts/Jone/AlexStateMachine/Base/Player.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// En esta clase se gestiona todo lo que tiene que ver con Alex y sus estados
+
+public class Player : MonoBehaviour
+{
+
+}
---
+++ Proyecto_01/Assets/Scripts/Jone/AlexStateMachine/StateMachine/PlayerState.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerState
+{
+    // Clase que define cómo serán los estados del jugador
+    
+    protected Player player;
+    protected PlayerStateMachine playerStateMachine;
+
+    public PlayerState(Player player, PlayerStateMachine playerStateMachine)
+    {
+        this.player = player;
+        this.playerStateMachine = playerStateMachine;
+    }
+
+    public virtual void EnterState() { }
+    public virtual void ExitState() { }
+    public virtual void FrameUpdate() { }
+    public virtual void PhysicsUpdate() { }
+
+    //public virtual void AnimationTriggerEvent(Enemy.AnimationTriggerType triggerType) { }
+}
