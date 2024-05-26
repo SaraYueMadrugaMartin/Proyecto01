3e354d34e371bf426ab2aff61e687353c6d8e552
Sara <smm1032@alu.ubu.es>
2024-05-27 00:31:56 +0200

    Aparece puzzle

---
+++ Proyecto_01/Assets/Scripts/Sara/MostrarPuzzle01.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MostrarPuzzle01 : MonoBehaviour
+{
+    [SerializeField] private Puzle puzle;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject panelPuzzle;
+    private bool jugadorTocando;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        panelPuzzle.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(jugadorTocando && Input.GetKeyDown(KeyCode.E))
+        {
+            Time.timeScale = 0f;
+            panelPuzzle.SetActive(true);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            jugadorTocando = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            jugadorTocando = false;
+        }
+    }
+
+    public void VerificarCodigoCompleto()
+    {
+        if (Puzle.totalNumerosCorrectos == 4)
+        {
+            Time.timeScale = 1f;
+            panelPuzzle.SetActive(false);
+        }
+        else
+        {
+            Debug.Log("Este no es el código correcto");
+        }
+    }
+}
