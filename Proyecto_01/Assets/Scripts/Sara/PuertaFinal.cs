be9aa6ba0d5959679159032d3397a98121b55c52
SaraYueMadrugaMartin <smm1032@alu.ubu.es>
2024-06-02 18:01:40 +0200

    Cambios min

--- Proyecto_01/Assets/Scripts/Sara/PuertaFinal.cs
+++ Proyecto_01/Assets/Scripts/Sara/PuertaFinal.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.VFX;
 
@@ -8,17 +9,23 @@ public class PuertaFinal : MonoBehaviour
     [SerializeField] private Inventario inventario;
     [SerializeField] private FadeAnimation fadeAnimation;
     [SerializeField] private GameObject panelFaltanLlaves;
+    [SerializeField] private TextMeshProUGUI textoLlaves;
 
     SFXManager sfxManager;
 
+
+
     private bool jugadorTocando = false;
     private bool todasLlaves = false;
+
+    private int contadorLlaves;
 
     // Start is called before the first frame update
     void Start()
     {
         sfxManager = FindObjectOfType<SFXManager>();
         panelFaltanLlaves.SetActive(false);
+        contadorLlaves = 3;
     }
 
     // Update is called once per frame
@@ -31,6 +38,12 @@ public class PuertaFinal : MonoBehaviour
             if (!todasLlaves)
             {
                 panelFaltanLlaves.SetActive(true);
+                if (contadorLlaves == 3)
+                    textoLlaves.text = "Faltan 3 llaves para desbloquear esta puerta";
+                else if(contadorLlaves == 2)
+                    textoLlaves.text = "Faltan 2 llaves para desbloquear esta puerta";
+                else if(contadorLlaves == 1)
+                    textoLlaves.text = "Falta 1 llave para desbloquear esta puerta";
             }
             else
             {
@@ -61,10 +74,13 @@ public class PuertaFinal : MonoBehaviour
     {
         if (inventario.TieneObjeto("LlaveX"))
         {
+            contadorLlaves--;
             if (inventario.TieneObjeto("LlaveY"))
             {
+                contadorLlaves--;
                 if (inventario.TieneObjeto("LlaveZ"))
                 {
+                    contadorLlaves--;
                     todasLlaves = true;
                 }
             }
@@ -78,6 +94,9 @@ public class PuertaFinal : MonoBehaviour
         
         sfxManager.PlaySFX(sfxManager.clipsDeAudio[13]);
         gameObject.SetActive(false);
+        inventario.VaciarHueco("LlaveX");
+        inventario.VaciarHueco("LlaveY");
+        inventario.VaciarHueco("LlaveZ");
         Time.timeScale = 1f;
     }
 }
