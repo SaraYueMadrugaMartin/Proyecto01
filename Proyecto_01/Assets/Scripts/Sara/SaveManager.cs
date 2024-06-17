1b0054b880ecd6e903c36c9cf71d2ae31426a3f0
SaraYueMadrugaMartin <smm1032@alu.ubu.es>
2024-06-17 18:16:45 +0200

    mierdolo

--- Proyecto_01/Assets/Scripts/Sara/SaveManager.cs
+++ Proyecto_01/Assets/Scripts/Sara/SaveManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using static UnityEditor.Progress;
 
 
 #region Toda informacion a guardar
